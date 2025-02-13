58f176f1fc4f7326f17824be2de40a81fe9376b0
ultrasecr.eth <241804+ultrasecreth@users.noreply.github.com>
2025-02-13 17:01:55 +0000

    Add mising pragma

--- generator.py
+++ generator.py
@@ -1,5 +1,5 @@
 base = """// SPDX-License-Identifier: MIT
-
+pragma solidity >=0.6.2 <0.9.0;
 library Solarray {{
     {}
 }}
--- src/Solarray.sol
+++ src/Solarray.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-
+pragma solidity >=0.6.2 <0.9.0;
 library Solarray {
     
     function uint8s(uint8 a) internal pure returns (uint8[] memory) {
